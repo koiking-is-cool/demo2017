c1d49aea1c95322d802e961d18d54ad856b3ad3f
nattakan <nattakan.eimo@bumail.net>
2017-02-04 16:48:20 +0700

    Week4

---
+++ Lab3/Assets/script/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	public Rigidbody rg;
+	public float speed;
+	public TextMesh txtBall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoJump(){
+		rg.velocity = new Vector3 (0,speed,0);
+	}
+	int cntCollosion = 0;
+	void OnCollosionEnt(Collision collision){
+		Debug.LogFormat ("collision is {0}",collision.gameObject.tag);
+		if (collision.gameObject.tag == "floor"){
+			cntCollosion++;
+			txtBall.text = "Floor : " + cntCollosion.ToString ();
+		}
+	}
+}
